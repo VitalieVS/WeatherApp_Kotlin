b0e44a251f292521e7fe0c2ff34f4550c644997b
VitalieVSV <dut66AHJCT>
2021-04-25 11:58:36 +0300

    Start

--- app/src/main/java/com/example/weatherapp_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp_kotlin/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState) //lala
         setContentView(R.layout.activity_main)
+        ///dasdasdasd
     }
 }
