1aeeebb9891ef1bb9c77194448e9ce8f418e1414
VitalieVSV <dut66AHJCT>
2021-04-25 12:21:02 +0300

    Hiding status bar and actionbar

--- app/src/main/java/com/example/weatherapp_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp_kotlin/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.weatherapp_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState) //hgfhgf
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.hide(); // hide actionBar
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN) // hide status bar
+
         setContentView(R.layout.activity_main)
+
     }
+
 }
